b3e7e6a6b66206e66db7a998d9935b332392d9b4
Лавріненко Олег Юрійович <OLavrinenko@corp.ukrtelecom.loc>
2016-10-12 17:17:12 +0300

    Try Django 1.9 - 11 of 38 - Request & Response

--- articles/admin.py
+++ articles/admin.py
@@ -4,6 +4,12 @@ from .models import Article
 # Register your models here.
 
 class ArticleAdmin(admin.ModelAdmin):
-	model = Article
+	list_display = ['title', 'category', 'author', 'views', 'create_date']
+	list_display_links = ['create_date']
+	list_editable = ['title']
+	list_filter = ['category', 'create_date']
+	search_fields = ['title', 'text']
+	class Meta:
+		model = Article
 
 admin.site.register(Article, ArticleAdmin)
--- articles/models.py
+++ articles/models.py
@@ -22,3 +22,6 @@ class Article(models.Model):
 
 	class Meta():
 		db_table = 'article'
+	
+	def __str__(self):
+		return self.title
